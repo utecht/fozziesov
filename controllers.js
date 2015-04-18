477f4ad033bdc71c7b240d90f8edb55e1cb69384
Joseph Utecht <thenmal@gmail.com>
2015-04-18 17:32:44 -0500

    added live updating version

--- controllers.js
+++ controllers.js
@@ -30,3 +30,14 @@ fozziesovApp.controller('BattleCtrl', function($scope) {
                 }
         ]};
 });
+
+fozziesovApp.controller('StratopCtrl', function($scope, $http, $interval) {
+   $http.post('/update').success(function(data) {
+       $scope.stratop = data;
+   }); 
+   $interval(function(){
+       $http.post('/update').success(function(data) {
+           $scope.stratop = data;
+       }); 
+   }, 100);
+});
