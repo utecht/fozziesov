edf77ea2df78fbf3828bd74b86f3a5b134e64148
Joseph Utecht <thenmal@gmail.com>
2015-04-18 18:04:28 -0500

    removed temp stuff from controller

--- static/controllers.js
+++ static/controllers.js
@@ -1,36 +1,5 @@
 var fozziesovApp = angular.module('fozziesovApp', []);
 
-fozziesovApp.controller('BattleCtrl', function($scope) {
-    $scope.stratop = {
-        'battles': [{
-            'sysname': 'GE-8JV',
-            'structname': 'Station',
-            'nodes': [{'name': 'NODE A', 'control': 'BRAVE'},
-                      {'name': 'NODE B', 'control': null},
-                      {'name': 'NODE C', 'control': 'TEST'}
-                     ]
-            }],
-        'status': {
-            'corps':  [{'name': 'TEST Alliance Please Ignore', 'num': 584},
-                       {'name': 'Brave Collective', 'num': 947} 
-                      ],
-            'events': [{'text': 'Cyno in SV-5'},
-                       {'text': 'Brave fleet moving to SV-5'}
-                        
-                      ]},
-        'systems': [
-               {'name': 'GE-8JV',
-                'corps': [{'name': 'TEST Alliance Please Ignore', 'num': 84},
-                          {'name': 'Brave Collective', 'num': 124} 
-                          ],
-                'events': [{'time': '21:43', 'text': 'dscan paste'},
-                           {'time': '21:38', 'text': 'dscan paste'},
-                           {'time': '20:23', 'text': 'dscan paste'}
-                           ]
-                }
-        ]};
-});
-
 fozziesovApp.controller('StratopCtrl', function($scope, $http, $interval) {
    $http.post('/test').success(function(data) {
        $scope.stratop = data;
@@ -39,5 +8,5 @@ fozziesovApp.controller('StratopCtrl', function($scope, $http, $interval) {
        $http.post('/test').success(function(data) {
            $scope.stratop = data;
        }); 
-   }, 100);
+   }, 1000);
 });
