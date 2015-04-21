efc898deb11b28406754291a348cec3a4fcaccc7
Joseph Utecht <joseph.utecht@arkansas.gov>
2015-04-21 16:25:35 -0500

    removed crest....

--- server/crest_app/views.py
+++ server/crest_app/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.views import login as django_login
 from django.views.generic import TemplateView
 from django.conf import settings
 
-import pycrest
 import requests
 from lxml import etree
 
@@ -21,21 +20,8 @@ def logout(request):
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-    def get_authed_crest_context(self):
-        """fetch some market data from authenticated CREST"""
-
-        # here we rudely fumble some of PyCrest's private parts
-        # since we already completed the authentication process via python-social-auth
-        authed_crest = pycrest.eve.AuthedConnection(
-            res=self.request.user._get_crest_tokens(),
-            endpoint=pycrest.EVE()._authed_endpoint,
-            oauth_endpoint=pycrest.EVE()._oauth_endpoint,
-            client_id=settings.SOCIAL_AUTH_EVEONLINE_KEY,
-            api_key=settings.SOCIAL_AUTH_EVEONLINE_SECRET
-        )
-        authed_crest()
-        print(authed_crest.whoami())
-        character_id = authed_crest.whoami()['CharacterID']
+    def get_authed_context(self):
+        character_id = self.request.user.character_id 
         r = requests.get('https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterID={}'.format(character_id))
         tree = etree.fromstring(r.content)
         c = tree.xpath('/eveapi/result/corporation')
@@ -58,5 +44,5 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['authed_crest'] = self.get_authed_crest_context()
+        context['authed_crest'] = self.get_authed_context()
         return context
