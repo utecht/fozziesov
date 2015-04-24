30f7bc55219d8f2903339a0f43c9d8457d569870
Joseph Utecht <joseph.utecht@arkansas.gov>
2015-04-24 16:23:49 -0500

    angular tied back in

---
+++ server/main/static/controllers.js
@@ -0,0 +1,9 @@
+var fozziesovApp = angular.module('fozziesovApp', []);
+
+fozziesovApp.controller('StratopCtrl', function($scope, $http, $interval) {
+   $interval(function(){
+       $http.post('/test').success(function(data) {
+           $scope.stratop = data;
+       }); 
+   }, 1000);
+});
